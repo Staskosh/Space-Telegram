cdab1e1ca7801de5cdd010e488de9e444cb50835
staskosh <staskoshenkov@rambler.ru>
2021-11-02 15:37:43 +0300

    Move the variable all_photos to the main function.

--- publish_images.py
+++ publish_images.py
@@ -6,11 +6,10 @@ import telegram
 from dotenv import load_dotenv
 
 
-def publish_images(directory, bot):
-  all_photos = listdir(directory)
+def publish_images(directory, bot, all_photos):
   while True:
     for index, photo in enumerate(all_photos):
-      time.sleep(3)
+      time.sleep(86400)
       with open(f'{directory}/{photo}', "rb") as photo:
           bot.send_photo(chat_id=os.getenv('TG_CHAT_ID'), photo=photo, timeout=100)
 
@@ -19,7 +18,8 @@ def main():
     load_dotenv()
     directory = os.getenv('PHOTO_FOLDER')
     bot = telegram.Bot(token=os.getenv('TG_TOKEN'))
-    publish_images(directory, bot)
+    all_photos = listdir(directory)
+    publish_images(directory, bot, all_photos)
 
 
 if __name__ == '__main__':
